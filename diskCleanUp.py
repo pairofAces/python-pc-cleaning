a05ef362ab4c9a053c304c0e4cd9df7542000bf6
pairofAces <karanchauhan16@gmail.com>
2021-03-02 20:59:07 -0500

    created an if statement to make sure the code will invoke when the downloaded file isn't in the 'Personal' folder

--- diskCleanUp.py
+++ diskCleanUp.py
@@ -9,4 +9,6 @@ class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         for filename in os.listdir(folder_to_track):
             i = 1
-            
+            if filename != "Personal":
+                new_name = filename
+                file_exists = os.path.isfile(folder_destination + '/' + new_name)
